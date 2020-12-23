308d188694f828d6aa68db78f1755a51f8f970d9
AlexGadd <gadda023@gmail.com>
2020-12-24 11:34:28 +1300

    Added more functions as well as trying to add an exit function, currently the exit function causes an infinite loop, otherwise working fine

--- App.py
+++ App.py
@@ -1,3 +1,6 @@
+books = []
+stay = True
+
 class Book:
     bookID = 0
     def __init__(self, title, author, length, target_date):
@@ -16,12 +19,51 @@ class Book:
     def __lt__(self, other):
         return self.bookID < other.bookID
 
-    def bookInfo(self):
-        return("Title: " + self.title + "\nAuthor: " + self.author + "\nNumber of pages: " + self.length + "\nTarget finish date: " + self.target_date + "\nCurrently Reading? " + "Yes" if self.current else "No")
+    def __str__(self):
+        return(f"Title: {self.title}\nAuthor: {self.author}\nNumber of pages: {self.length}\nTarget finish date: {self.target_date}\nCurrently Reading? {'Yes' if self.current else 'No'}")
 
-book1 = Book(input("please enter a title: "), input("please enter the author's name: "), input("Please enter the number of pages"), input("please enter the target date"))
+    def getBookName(self):
+        return(f"{self.title} by {self.author}")
 
-print(book1.bookInfo())
+def exitchecker(answer):
+    typed = input(answer)
+    if typed == "exit" or typed == 0:
+        stay = False
+        exit()
+    else:
+        return typed
 
-# choice = input("Would you like to: \n1. Start a new book\n2.Add a new page position to a book you're reading\n3.View stats on a previously read book?\n")
-# "\nCurrent position: " + self.position +
+
+
+
+def newBook():
+    books.append(Book(exitchecker("please enter a title: "), exitchecker("please enter the author's name: "), exitchecker("Please enter the number of pages: "), exitchecker("please enter the target date: ")))
+
+def listAllBooks():
+    for x in books:
+        print(x)
+
+def listTitles():
+    print("Please choose a title:")
+    for count, value in enumerate(books):
+        print(f"{count + 1}. {value.getBookName()}")
+
+
+
+def mainMenu():
+    choice = 0
+    while stay:
+        try:
+            choice = int(exitchecker("\nWould you like to: \n1. Start a new book\n2.Add a new page position to a book you're reading\n3.View stats on a previously read book?\n"))
+        except:
+            print("\n\n!Please enter a number between 1 and 3!")
+
+        if choice == 1:
+            newBook()
+        elif choice == 2:
+            listTitles()
+        elif choice == 3:
+            pass
+
+
+mainMenu()
